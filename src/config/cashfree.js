6362ec03f408ab7823a47dd48e4e771329fb1247
quicbinary <quicbinary@gmail.com>
2025-11-29 14:40:09 +0530

    production

--- src/config/cashfree.js
+++ src/config/cashfree.js
@@ -3,5 +3,5 @@ dotenv.config();
 
 export const appId = process.env.CASHFREE_APP_ID;
 export const secretKey = process.env.CASHFREE_SECRET_KEY;
-export const isProduction = false;
-export const baseUrl = "https://sandbox.cashfree.com/pg";
+export const isProduction = true;
+export const baseUrl = "https://api.cashfree.com/pg";
